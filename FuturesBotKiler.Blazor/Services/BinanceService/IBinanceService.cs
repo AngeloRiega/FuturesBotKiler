5c72fb033d8c8b3a417b273d3796cb1c8e726d6f
AngeloRiega <angelo.riega.iriarte@gmail.com>
2023-03-15 16:24:57 -0500

    Cambio nombre proyecto

---
+++ FuturesBotKiler.Blazor/Services/BinanceService/IBinanceService.cs
@@ -0,0 +1,15 @@
+﻿using FuturesBotKiler.Shared.Models;
+
+namespace FuturesBotKiler.Blazor.Services.BinanceService
+{
+    public interface IBinanceService
+    {
+        //List<Position> Positions { get; set; }
+        //Position Position { get; set; }
+        //Task<Position> GetPosition(string Symbol);
+        //Task GetPositions(string Symbol);
+        Task<List<Position>> GetPositions(List<string> Symbol);
+        Task<bool> ClosePosition(string Symbol);
+        Task<bool> NewPosition(string symbol, bool side, decimal size, decimal porcentajeStopLoss, decimal porcentajeTakeProfit);
+    }
+}
---
+++ FuturesBotKiler.Blazor/Services/PositionService/PositionService.cs
@@ -0,0 +1,19 @@
+﻿namespace FuturesBotKiler.Blazor.Services.PositionService
+{
+    public class PositionService
+    {
+        public List<string> Watchlist = new List<string>();
+
+        //TODO: NO REPETIDOS EN EL WATCHLIST (PASARLO A DICCIONARIO)
+
+        public void AddWatchlist(string symbol)
+        {
+            Watchlist.Add(symbol);
+        }
+
+        public void DeleteWatchlist(string symbol)
+        {
+            Watchlist.Remove(symbol);
+        }
+    }
+}
